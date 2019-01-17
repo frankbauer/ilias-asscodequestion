6f61d91beff1a5d92dcac959d83b9678ba4f2df2
Bernhard Liebl <Bernhard.Liebl@rz.uni-regensburg.de>
2019-01-17 09:46:45 +0100

    Fix title row highlighting in xls export.

--- classes/class.assCodeQuestion.php
+++ classes/class.assCodeQuestion.php
@@ -757,8 +757,9 @@ class assCodeQuestion extends assQuestion implements ilObjQuestionScoringAdjusta
 		}
 
 		if ($il52){
-			$worksheet->setCell($startrow, 0, $this->plugin->txt($this->getQuestionType()));
-			$worksheet->setCell($startrow, 1, $this->getTitle());	
+			// also see parent::setExportDetailsXLS($worksheet, $startrow, $active_id, $pass);
+			$worksheet->setFormattedExcelTitle($worksheet->getColumnCoord(0) . $startrow, $this->plugin->txt($this->getQuestionType()));
+			$worksheet->setFormattedExcelTitle($worksheet->getColumnCoord(1) . $startrow, $this->getTitle());
 		} else {
 			$worksheet->writeString($startrow, 0, ilExcelUtils::_convert_text($this->plugin->txt($this->getQuestionType())), $format_title);
 			$worksheet->writeString($startrow, 1, ilExcelUtils::_convert_text($this->getTitle()), $format_title);
