17d7502a36b9c16a21b39c46b17b22772da8c4ad
Fred Neumann <fred.neumann@fau.de>
2019-07-17 15:50:40 +0200

    Version for ILIAS 5.4. Compatibility with 5.3 and below is broken due to signature change in getSpecificFeedbackOutput()

--- classes/class.assCodeQuestionGUI.php
+++ classes/class.assCodeQuestionGUI.php
@@ -628,15 +628,18 @@ class assCodeQuestionGUI extends assQuestionGUI implements ilGuiQuestionScoringA
 
 	/**
 	 * Returns the answer specific feedback for the question
-	 * 
-	 * @param integer $active_id Active ID of the user
-	 * @param integer $pass Active pass
+	 *
+	 * This method should be overwritten by the actual question.
+	 *
+	 * @todo Mark this method abstract!
+	 * @param array $userSolution ($userSolution[<value1>] = <value2>)
 	 * @return string HTML Code with the answer specific feedback
 	 * @access public
 	 */
-	function getSpecificFeedbackOutput($active_id, $pass)
+	function getSpecificFeedbackOutput($userSolution)
 	{
 		// By default no answer specific feedback is defined
+		$output = '';
 		return $this->object->prepareTextareaOutput($output, TRUE);
 	}
 	
