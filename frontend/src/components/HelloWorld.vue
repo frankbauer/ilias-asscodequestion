8a847cadd5a9210e7ce6bf4f616517f7e7bfa279
Frank Bauer <frank.abuer@fau.de>
2019-11-11 20:45:40 +0100

    Link to a codemirror github with examples

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -26,6 +26,7 @@
 </template>
 
 <script>
+//https://github.com/surmon-china/vue-codemirror
 import codemirror from 'vue-codemirror'
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/theme/solarized.css'
