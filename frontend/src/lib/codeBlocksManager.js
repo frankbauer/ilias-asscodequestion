fe7ddc604cf391c7504ec96910262487d4339baf
Frank Bauer <frank.abuer@fau.de>
2019-12-07 02:05:45 +0100

    fixed yet another version bug

--- frontend/src/lib/codeBlocksManager.js
+++ frontend/src/lib/codeBlocksManager.js
@@ -119,7 +119,7 @@ class CodeBlocksManager {
                 block.width = bl.getAttribute('width')?bl.getAttribute('width'):block.width
                 block.height = bl.getAttribute('height')?bl.getAttribute('height'):block.height
                 block.align = bl.getAttribute('align')?bl.getAttribute('align'):block.align   
-                block.version = bl.getAttribute('version')?bl.getAttribute('version'):'101'
+                block.version = bl.getAttribute('data-version')?bl.getAttribute('data-version'):'101'
             } else if (block.type == 'BLOCK') {
                 block.hasCode = true;                
             } else if (block.type == 'LOADING' || block.type == 'DIV') {
