1a99e70fe13ed88d33a5caf4b5161ebf1438d42f
Frank Bauer <frank.abuer@fau.de>
2019-12-09 16:02:49 +0100

    added first deployment version

---
+++ codeblocks/0.1.0/js/doppio/v001/doppio/natives/sun_net.js
@@ -0,0 +1,2 @@
+"use strict";var sun_net_spi_DefaultProxySelector=function(){function a(){}return a["init()Z"]=function(a){return!0},a["getSystemProxy(Ljava/lang/String;Ljava/lang/String;)Ljava/net/Proxy;"]=function(a,b,c,d){return null},a}();registerNatives({"sun/net/spi/DefaultProxySelector":sun_net_spi_DefaultProxySelector});
+//# sourceMappingURL=sun_net.js.map
