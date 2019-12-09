6c0ae37b7523a1cb49f8b45b364eb38fa55fde81
Frank Bauer <frank.abuer@fau.de>
2019-12-09 17:08:34 +0100

    fixed version string error

--- codeblocks/0.1.0/ilias-conf.php
+++ codeblocks/0.1.0/ilias-conf.php
@@ -1,5 +1,5 @@
 <?php 
-define("CODEBLOCKS_VERSION",     ".0.1.0");
+define("CODEBLOCKS_VERSION",     "0.1.0");
 define("CODEBLOCKS_BASE_URI",     "./Customizing/global/plugins/Modules/TestQuestionPool/Questions/assCodeQuestion/codeblocks/0.1.0/");
 define("CODEBLOCKS_REL_PATH",     "codeblocks/0.1.0/");
 ?> 
