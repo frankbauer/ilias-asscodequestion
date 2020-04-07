798f028f49f38d83e8a8e38913583df3dff9aa8c
Frank Bauer <frank.bauer@fau.de>
2020-04-07 16:40:20 +0200

    Removed Trailing spaces from codeblocks config

--- classes/support/codeblocks-conf-0.2.1.php
+++ classes/support/codeblocks-conf-0.2.1.php
@@ -3,4 +3,4 @@ define("CODEBLOCKS_VERSION",     "0.2.1");
 define("CODEBLOCKS_BASE_URI",     ".//Customizing/global/plugins/Modules/TestQuestionPool/Questions/assCodeQuestion/codeblocks/0.2.1/");
 define("CODEBLOCKS_REL_PATH",     "codeblocks/0.2.1/");
 define("CODEBLOCKS_TAG_REGEX",     "/{:([\w]+)}/");
-?> 
+?>
