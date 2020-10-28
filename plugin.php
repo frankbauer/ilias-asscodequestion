7b4dd930df34be7ceaa3ded092e94324c0ceb48f
Frank Bauer <frank.bauer@fau.de>
2020-10-28 17:29:25 +0100

    updated min/max versions

--- plugin.php
+++ plugin.php
@@ -8,8 +8,8 @@ $version = "2.0.1";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "5.4";
-$ilias_max_version = "5.4";
+$ilias_min_version = "5.4.0";
+$ilias_max_version = "5.4.18";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Frank Bauer";
