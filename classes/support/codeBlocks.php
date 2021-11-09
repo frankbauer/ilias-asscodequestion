ffd1a1d3516c7af32417dfbb1bbdc3e5e6508487
Frank Bauer <frank.bauer@fau.de>
2021-11-09 16:16:43 +0100

    Updated codeblocks to fix Racecondition

--- classes/support/codeBlocks.php
+++ classes/support/codeBlocks.php
@@ -17,7 +17,7 @@ class codeBlocks implements ArrayAccess {
 		$this->id = $id;
 		$this->getPlugin()->includeClass("./ui/codeBlocksUI.php");
 		$this->getPlugin()->includeClass("./support/codeBlock.php");
-		$this->getPlugin()->includeClass("./support/codeblocks-conf-0.2.10.php");		
+		$this->getPlugin()->includeClass("./support/codeblocks-conf-0.2.11.php");		
 
 		if ($json_data == null){
 			$this->additional_data = array();
