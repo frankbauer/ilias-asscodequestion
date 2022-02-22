1ad34a81b96c91e9c142cb2c7ec22dac26b23529
Frank Bauer <frank.bauer@fau.de>
2022-02-22 09:10:57 +0100

    Support for Ilias 7 (#10)

--- plugin.php
+++ plugin.php
@@ -4,12 +4,12 @@
 $id = "codeqst";
 
 // code version; must be changed for all code changes
-$version = "2.0.2";
+$version = "2.0.3";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "5.4.0";
-$ilias_max_version = "5.4.999";
+$ilias_max_version = "7.999";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Frank Bauer";
