a9e01ea92305a1f632a7e2abfee62eb1fa9887f4
Frank Bauer <frank.bauer@fau.de>
2022-05-05 16:06:37 +0200

    Updated Version

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
 $id = "codeqst";
 
 // code version; must be changed for all code changes
-$version = "2.0.3";
+$version = "2.0.4";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
